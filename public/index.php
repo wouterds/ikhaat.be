25bf2df29f264f37519ce36302956780a6c1e8bd
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-10-03 21:40:38 +0200

    :bulb: Moved comment

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 
-// Application directory
 use Wouterds\IkHaat\Application\Http\Application;
 
+// Application directory
 define('APP_DIR', dirname(__DIR__));
 
 // Include composer autoloader
