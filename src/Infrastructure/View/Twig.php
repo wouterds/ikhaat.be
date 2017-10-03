d07437a3ca6f3d83311630b3d6e49cb3fe5ddcaf
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-10-03 22:09:04 +0200

    :sparkles: Added renderWithResponse

--- src/Infrastructure/View/Twig.php
+++ src/Infrastructure/View/Twig.php
@@ -2,8 +2,25 @@
 
 namespace Wouterds\IkHaat\Infrastructure\View;
 
+use Slim\Http\Response;
 use Twig_Environment;
 
 class Twig extends Twig_Environment
 {
+    /**
+     * @param Response $response
+     * @param $name
+     * @param array $context
+     * @return Response
+     */
+    public function renderWithResponse(Response $response, $name, array $context = []): Response
+    {
+        // Render template
+        $contents = $this->render($name, $context);
+
+        // Write contents to response
+        $response->getBody()->write($contents);
+
+        return $response;
+    }
 }
