8cd047e467989407a4977348812d5f4ae57bbdfe
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-10-03 22:10:50 +0200

    :sparkles: Extend from abstract ViewHandler

--- src/Application/Http/Handlers/HomeHandler.php
+++ src/Application/Http/Handlers/HomeHandler.php
@@ -2,35 +2,13 @@
 
 namespace Wouterds\IkHaat\Application\Http\Handlers;
 
-use Slim\Http\Request;
-use Slim\Http\Response;
-use Wouterds\IkHaat\Infrastructure\View\Twig;
-
-class HomeHandler
+class HomeHandler extends ViewHandler
 {
     /**
-     * @var Twig
+     * @return string
      */
-    private $twig;
-
-    /**
-     * @param Twig $twig
-     */
-    public function __construct(Twig $twig)
+    public function getTemplate(): string
     {
-        $this->twig = $twig;
-    }
-
-    /**
-     * @param Request $request
-     * @param Response $response
-     * @return Response
-     */
-    public function __invoke(Request $request, Response $response): Response
-    {
-        $contents = $this->twig->render('home.html.twig');
-        $response->getBody()->write($contents);
-
-        return $response;
+        return 'home.html.twig';
     }
 }
