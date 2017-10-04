b6046e48a64deb27a7a532b7a20b322db09b9488
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-10-04 12:24:19 +0200

    :fire: Removed dead code

--- public/index.php
+++ public/index.php
@@ -9,9 +9,6 @@ define('APP_DIR', dirname(__DIR__));
 // Include composer autoloader
 require_once (APP_DIR . '/vendor/autoload.php');
 
-// Load container
-$container = Container::load();
-
 // Init http app
 $app = new Application();
 
