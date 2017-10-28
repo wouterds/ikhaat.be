f2570064f4d9055f0f7f64483348e7ec4b77f6a3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2017-10-28 16:41:06 +0200

    :sparkles: Fake page with meta data for Twitter crawler

---
+++ src/Application/Http/Handlers/TwitterBotImageHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Wouterds\KabouterWesley\Application\Http\Handlers;
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class TwitterBotImageHandler extends ViewHandler
+{
+    /**
+     * @return string
+     */
+    public function getTemplate(): string
+    {
+        return 'twitter-bot.html.twig';
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response): Response
+    {
+        // BUG: We should be able to get this from attributes
+        $content = $request->getUri()->getPath();
+        $content = explode('.jpg', $content);
+        $content = reset($content);
+        $content = substr($content, 1, strlen($content) - 1);
+        $content = urldecode($content);
+
+        return $this->render($response, [
+            'title' => $content . ' - ' . getenv('APP_BASE_NAME'),
+            'content' => $content,
+            'twitterAccount' => getenv('TWITTER_ACCOUNT'),
+            'twitterImage' => getenv('APP_URL') . $request->getUri()->getPath(),
+            'url' => getenv('APP_URL'),
+        ]);
+    }
+}
