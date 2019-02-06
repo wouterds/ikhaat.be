cee1dc59c992cd882ff0124d4d21d0912e599cfa
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-02-06 09:04:44 +0100

    :construction: Comented out old code

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -1,78 +1,46 @@
-const $wrapper = $('#wrapper');
+// this.APP_URL = this.$scope.data('app-url');
+// this.clipboard = new Clipboard(this.$urlInput.get(0));
 
-class App {
-  constructor($scope) {
-    this.$scope = $scope;
+// inputOnKeyup(e) {
+//   let val = this.$input.val();
 
-    this.init();
-    this.initObjects();
-    this.initObservers();
+//   // No value?
+//   if (val.length === 0) {
+//     // Use the one from the placeholder!
+//     val = this.$input.attr('placeholder');
+//   }
 
-    // Trigger input once to pre-fill url input
-    this.$input.trigger('keyup');
-  }
+//   val = val.toLowerCase();
 
-  init() {
-    this.APP_URL = this.$scope.data('app-url');
-    this.$input = this.$scope.find("#input");
-    this.$image = this.$scope.find("#image");
-    this.$urlInputWrapper = this.$scope.find('#urlInputWrapper');
-    this.$urlInput = this.$urlInputWrapper.find('#urlInput');
-  }
+//   let url = this.APP_URL + '/' + encodeURIComponent(val) + '.jpg';
 
-  initObjects() {
-    this.clipboard = new Clipboard(this.$urlInput.get(0));
-  }
+//   this.$image.attr('src', url);
+//   this.$image.attr('alt', val);
+//   this.$urlInput.val(url);
+//   this.$urlInputWrapper.removeClass('copied-to-clipboard');
+//   this.removeCopiedNotice();
+// }
 
-  initObservers() {
-    this.$input.on('keyup', ::this.inputOnKeyup);
-    this.$urlInput.on('click', ::this.urlInputClick);
-    this.clipboard.on('success', ::this.clipboardSuccess);
-  }
+// urlInputClick(e) {
+//   if (Clipboard.isSupported()) {
+//     return;
+//   }
 
-  inputOnKeyup(e) {
-    let val = this.$input.val();
+//   this.$urlInput.select();
+// }
 
-    // No value?
-    if (val.length === 0) {
-      // Use the one from the placeholder!
-      val = this.$input.attr('placeholder');
-    }
+// clipboardSuccess(e) {
+//   this.$urlInputWrapper.addClass('copied-to-clipboard');
+//   this.removeCopiedNoticeTimer = setTimeout(::this.removeCopiedNotice, 5000);
+// }
 
-    val = val.toLowerCase();
+// removeCopiedNotice() {
+//   this.$urlInput.blur();
+//   this.$urlInputWrapper.removeClass('copied-to-clipboard');
 
-    let url = this.APP_URL + '/' + encodeURIComponent(val) + '.jpg';
+//   if (this.removeCopiedNoticeTimer) {
+//     clearTimeout(this.removeCopiedNoticeTimer);
+//   }
 
-    this.$image.attr('src', url);
-    this.$image.attr('alt', val);
-    this.$urlInput.val(url);
-    this.$urlInputWrapper.removeClass('copied-to-clipboard');
-    this.removeCopiedNotice();
-  }
-
-  urlInputClick(e) {
-    if (Clipboard.isSupported()) {
-      return;
-    }
-
-    this.$urlInput.select();
-  }
-
-  clipboardSuccess(e) {
-    this.$urlInputWrapper.addClass('copied-to-clipboard');
-    this.removeCopiedNoticeTimer = setTimeout(::this.removeCopiedNotice, 5000);
-  }
-
-  removeCopiedNotice() {
-    this.$urlInput.blur();
-    this.$urlInputWrapper.removeClass('copied-to-clipboard');
-
-    if (this.removeCopiedNoticeTimer) {
-      clearTimeout(this.removeCopiedNoticeTimer);
-    }
-
-    this.removeCopiedNoticeTimer = null;
-  }
-}
-
-new App($wrapper);
+//   this.removeCopiedNoticeTimer = null;
+// }
