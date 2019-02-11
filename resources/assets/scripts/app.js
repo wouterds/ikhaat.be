03ad0f1d83b4c43f6fd2f16dde9887a2a05fd2e5
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-02-11 23:33:06 +0100

    :recycle: Refactored to plain js

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -1,46 +1,69 @@
-// this.APP_URL = this.$scope.data('app-url');
-// this.clipboard = new Clipboard(this.$urlInput.get(0));
+import Clipboard from 'clipboard';
+import '../styles/app.scss';
 
-// inputOnKeyup(e) {
-//   let val = this.$input.val();
+const wrapper = document.getElementById('wrapper');
+const input = document.getElementById('input');
+const image = document.getElementById('image');
+const urlInput = document.getElementById('urlInput');
+const urlInputWrapper = document.getElementById('urlInputWrapper');
+const copiedToClipboardMessage = document.getElementById('copiedToClipboardMessage');
 
-//   // No value?
-//   if (val.length === 0) {
-//     // Use the one from the placeholder!
-//     val = this.$input.attr('placeholder');
-//   }
+let removeCopiedNoticeTimer = null;
+const removeCopiedNotice = () => {
+  urlInput.blur();
+  urlInputWrapper.classList.remove('copied-to-clipboard');
 
-//   val = val.toLowerCase();
+  if (removeCopiedNoticeTimer) {
+    clearTimeout(removeCopiedNoticeTimer);
+  }
 
-//   let url = this.APP_URL + '/' + encodeURIComponent(val) + '.jpg';
+  removeCopiedNoticeTimer = null;
+};
 
-//   this.$image.attr('src', url);
-//   this.$image.attr('alt', val);
-//   this.$urlInput.val(url);
-//   this.$urlInputWrapper.removeClass('copied-to-clipboard');
-//   this.removeCopiedNotice();
-// }
+const inputOnKeyUp = e => {
+  e.preventDefault();
 
-// urlInputClick(e) {
-//   if (Clipboard.isSupported()) {
-//     return;
-//   }
+  let { value } = input;
 
-//   this.$urlInput.select();
-// }
+  // No value?
+  if (value.length === 0) {
+    value = input.getAttribute('placeholder');
+  }
 
-// clipboardSuccess(e) {
-//   this.$urlInputWrapper.addClass('copied-to-clipboard');
-//   this.removeCopiedNoticeTimer = setTimeout(::this.removeCopiedNotice, 5000);
-// }
+  // Always lowercase
+  value = value.toLowerCase();
 
-// removeCopiedNotice() {
-//   this.$urlInput.blur();
-//   this.$urlInputWrapper.removeClass('copied-to-clipboard');
+  // Compose url
+  const url = `${wrapper.getAttribute('data-app-url')}/${encodeURIComponent(value)}.jpg`;
 
-//   if (this.removeCopiedNoticeTimer) {
-//     clearTimeout(this.removeCopiedNoticeTimer);
-//   }
+  image.setAttribute('src', url);
+  image.setAttribute('alt', value);
+  urlInput.value = url;
 
-//   this.removeCopiedNoticeTimer = null;
-// }
+  removeCopiedNotice();
+};
+
+const inputOnClick = e => {
+  e.preventDefault();
+
+  // If clipboard works, stop here
+  if (Clipboard.isSupported()) {
+    urlInputWrapper.classList.add('copied-to-clipboard');
+    copiedToClipboardMessage.innerText = copiedToClipboardMessage.getAttribute('data-text');
+    removeCopiedNoticeTimer = setTimeout(removeCopiedNotice, 3000);
+    return;
+  }
+
+  // Else select contents of input
+  urlInput.setSelectionRange(0, urlInput.value.length);
+};
+
+// Event listeners
+input.addEventListener('keyup', inputOnKeyUp);
+urlInput.addEventListener('click', inputOnClick);
+
+// Trigger initial event
+input.dispatchEvent(new KeyboardEvent('keyup'));
+
+// Clipboard plugin
+new Clipboard(urlInput);
